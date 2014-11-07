29b7ccfd24847f47e07023cbc4f71ca10f0cb81a
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:39:26 +0100

    Cleanup tests

--- tests/test_sale_opportunity_talk.py
+++ tests/test_sale_opportunity_talk.py
@@ -2,37 +2,23 @@
 # This file is part of the sale_opportunity_talk module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class SaleOpportunityTalkTestCase(unittest.TestCase):
-    '''
-    Test Sale Opportunity Talk module.
-    '''
+    'Test Sale Opportunity Talk module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_opportunity_talk')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_opportunity_talk')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
